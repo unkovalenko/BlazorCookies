ca8f626af6fef0e9fcf51edb11f3e183b087a8ed
unk <unkovalenko@gmail.com>
2021-04-27 21:33:49 +0300

    cullture continue

---
+++ Blazor.Learner/Client/ColumnCollections.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GridShared;
+
+using GridBlazor.Resources;
+
+using GridShared.Columns;
+using GridShared.Utility;
+
+using System.Threading.Tasks;
+
+
+
+
+using BlazorCookies.Models;
+
+
+    namespace BlazorCookies.Shared
+    {
+        public class ColumnCollections
+        {
+            public const string CompanyNameFilter = "CompanyNameFilter";
+
+            public static Action<IGridColumnCollection<EXCHCHECK>> ExchcheckColumns = c =>
+            {
+                c.Add(o => o.EC_ID).SetPrimaryKey(true).Titled("ID").SetWidth(10).Max(true);
+                c.Add(o => o.EC_DATE, "OrderCustomDate").Titled(Constants.DATE).Min(true)
+                //.SetWidth(120).RenderComponentAs<TooltipCell>()
+                ;
+                c.Add(o => o.EC_REM).Titled(Constants.REM).SetWidth(250);
+               // c.Add(o => o.EC_ID).Encoded(false).Sanitized(false)
+               //  .RenderValueAs(o => $"<div class='btn-group'><button type = 'button' class='btn btn-primary' @onclick='ButtonClick' >Apple</button> <button type = 'button' class='btn btn-primary'>Samsung</button>  <button type = 'button' class='btn btn-primary'>Sony</button></div>");
+
+
+            };
+        }
+    }
